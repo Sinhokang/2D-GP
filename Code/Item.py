46e32498979f367550015ebe1f7ddf150824b099
Sinhokang <ksh-1063@hanmail.net>
2016-10-28 16:11:16 +0900

    10-28

--- Code/Item.py
+++ Code/Item.py
@@ -10,14 +10,14 @@ class Item_bomb:
     #monster=Monster()
     def __init__(self):
         self.x,self.y=50,100
-        self.nx,self.ny=50,250
-        self.frame=2
+        self.nx,self.ny=50,150
+        self.frame=0
         self.image=load_image('../Resource/object/item_bomb.png')
         self.Num=load_image('../Resource/ui/Number.png')
 
     def draw(self):
         self.image.draw(self.x,self.y)
-        self.Num.clip_draw(self.frame*100,0,100,100,self.nx,self.ny)
+        self.Num.clip_draw(self.frame*100,0,40,20,self.nx,self.ny)
 
     def handle_event(self, event):
         if event.type == SDL_KEYDOWN and event.key == SDLK_z:
@@ -28,11 +28,16 @@ class Item_bomb:
 class Item_slow:
     def __init__(self):
         self.x, self.y = 750, 100
+        self.nx, self.ny = 750, 150
+        self.frame=0
         self.image = load_image('../Resource/object/item_break.png')
-
+        self.Num = load_image('../Resource/ui/Number.png')
 
     def draw(self):
         self.image.draw(self.x, self.y)
+        self.Num.clip_draw(self.frame * 100, 0, 40, 20, self.nx, self.ny)
 
-   # def handle_event(self, event):
-      #  if event.type == SDL_KEYDOWN:
+
+    def handle_event(self, event):
+        if event.type == SDL_KEYDOWN and event.key == SDLK_x:
+            self.update()
