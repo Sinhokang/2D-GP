6d076714d017d3ac5ac96c6fdee9719290ee2f3c
Sinhokang <ksh-1063@hanmail.net>
2016-11-01 15:19:34 +0900

    11-01

--- Code/Item.py
+++ Code/Item.py
@@ -11,18 +11,18 @@ class Item_bomb:
     def __init__(self):
         self.x,self.y=50,100
         self.nx,self.ny=50,150
-        self.frame=0
+        self.frame=2
         self.image=load_image('../Resource/object/item_bomb.png')
-        self.Num=load_image('../Resource/ui/Number.png')
+        self.Num=load_image('../Resource/ui/Number01.png')
 
     def draw(self):
         self.image.draw(self.x,self.y)
-        i
-        self.Num.clip_draw(self.frame*100,0,40,20,self.nx,self.ny)
+
+        self.Num.clip_draw(self.frame*35,0,40,20,self.nx,self.ny)
 
     def handle_event(self, event):
         if event.type == SDL_KEYDOWN and event.key == SDLK_z:
-            self.update()
+            self.frame -=1
 
 
 
@@ -30,15 +30,15 @@ class Item_slow:
     def __init__(self):
         self.x, self.y = 750, 100
         self.nx, self.ny = 750, 150
-        self.frame=0
+        self.frame=2
         self.image = load_image('../Resource/object/item_break.png')
-        self.Num = load_image('../Resource/ui/Number.png')
+        self.Num = load_image('../Resource/ui/Number01.png')
 
     def draw(self):
         self.image.draw(self.x, self.y)
-        self.Num.clip_draw(self.frame * 100, 0, 40, 20, self.nx, self.ny)
+        self.Num.clip_draw(self.frame * 35, 0, 40, 20, self.nx, self.ny)
 
 
     def handle_event(self, event):
         if event.type == SDL_KEYDOWN and event.key == SDLK_x:
-            self.update()
+            self.frame -= 1
