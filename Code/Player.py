e8cc9f2eb4e9743b5552460c482670effdbcaf7f
Sinhokang <ksh-1063@hanmail.net>
2016-12-03 18:35:28 +0900

    12-03

--- Code/Player.py
+++ Code/Player.py
@@ -7,8 +7,8 @@ from pico2d import *
 
 class aircraft:
 
-    PIXEL_PER_METER = (20.0 / 1)
-    RUN_SPEED_KMPH = 100.0
+    PIXEL_PER_METER = (10.0 / 0.3)
+    RUN_SPEED_KMPH = 30.0
     RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
     RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
     RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
